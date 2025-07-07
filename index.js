51e3d63d1ee9c66910ec46cd148aa39b08e0c2bc
DylanTran0 <tavan.iuyouth@gmail.com>
2025-07-07 17:54:04 +0700

    Refactor HTML and CSS: Add global styles, improve typography, and enhance button interactions. Update HTML structure with proper doctype and meta tags for better accessibility and responsiveness.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+const saveEl = document.getElementById("save-el")
+const countEl = document.getElementById("count-el")
+const incrementBtn = document.getElementById('increment-btn')
+const saveBtn = document.getElementById('save-btn')
 let count = 0
 
-function increment() {
-    count += 1
+incrementBtn.addEventListener('click', function(){
+    count++
     countEl.textContent = count
-}
+})
 
-function save() {
+saveBtn.addEventListener('click', function(){
     let countStr = count + " - "
     saveEl.textContent += countStr
-    countEl.textContent = 0
     count = 0
-}
+    countEl.textContent = count
+})
